072641049d48ae4ad12546a9dfcbe29c912be14e
madforlife <madforlifee@gmail.com>
2025-03-29 11:13:09 +0200

    Initial

---
+++ src/main/java/com/pros/prosassignment/ProsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.pros.prosassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProsAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pros/prosassignment/ProsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pros.prosassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
